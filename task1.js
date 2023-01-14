6b4b24dd593e4e229896272b2b4024cf3302823a
Azamat Murzaliev <lirog.kg@gmail.com>
2023-01-14 18:07:38 +0600

    Сделал второе задание
    Шифровка и расшифровка текста методом Виженера с помощью express

--- task1.js
+++ task1.js
@@ -6,6 +6,18 @@ app.get('/:something', (req, res) => {
   res.send(req.params.something);
 });
 
+const Vigenere = require('caesar-salad').Vigenere;
+
+app.get('/encode/:something', (req, res) => {
+  const message = Vigenere.Cipher('password').crypt(req.params.something);
+  res.send(message);
+});
+
+app.get('/decode/:something', (req, res) => {
+  const message = Vigenere.Decipher('password').crypt(req.params.something);
+  res.send(message);
+});
+
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
